1e8d90c6712f0ef1fdf37fd88adbe9effcb67725
Syuzanna2004 <83169222+Syuzanna2004@users.noreply.github.com>
2022-04-01 16:20:04 +0400

    Add files via upload

---
+++ partials-front/footer.php
@@ -0,0 +1,82 @@
+ <!-- social Section Starts Here -->
+ <footer class="footer">
+	<div class="container">
+		<div class="footer__inner">
+			<div class="footer-contact">
+				<a class="footer-contact--logo" href="#">
+					<img src="img/Logotip.svg" alt="">
+				</a>
+				<ul class="footer-contact__list">
+					<li class="footer-contact__item">
+						<a class="footer-contact__link footer-contact__link--address" href="#">г. Самара, ул. Уличная, дом 88</a>
+					</li>
+					<li class="footer-contact__item">
+						<a class="footer-contact__link footer-contact__link--phone" href="tel:+89242828832">888-88-88</a>
+					</li>
+					<li class="footer-contact__item">
+						<a class="footer-contact__link footer-contact__link--mail" href="#">saleofplants@mail.ru</a>
+					</li>
+				</ul>
+			</div>
+			<div class="footer-about">
+				<h6 class="footer-about__title footer__title">О компании</h6>
+				<div class="footer-border"></div>
+				<nav class="footer-menu">
+					<ul class="footer-menu__list">
+						<li class="footer-menu__item">
+							<a class="footer-menu__link" href="#">Каталог</a>
+						</li>
+						<li class="footer-menu__item">
+							<a class="footer-menu__link" href="#">Выгодные предложения</a>
+						</li>
+						<li class="footer-menu__item">
+							<a class="footer-menu__link" href="#">Рекомендации</a>
+						</li>
+						<li class="footer-menu__item">
+							<a class="footer-menu__link" href="#">Наши приемущества</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<div class="footer-notifications">
+				<h6 class="footer-notifications__title footer__title">Рассылка</h6>
+				<div class="footer-border"></div>
+				<p class="footer-notifications__text">Получайте уведомления о скидках, акциях новых
+					товарах и выгодных предложениях.
+				</p>
+				<form class="footer-form">
+					<div class="footer-form__container">
+						<input class="footer-form__input" type="email" placeholder="Введите Email">
+						<button class="footer-form__button" type="submit"></button>
+					</div>
+				</form>
+			</div>
+			<div class="footer-social">
+				<h6 class="footer-social__title footer__title">Соц. сети</h6>
+				<div class="footer-border"></div>
+				<p class="footer-social__text">Следите за нами.</p>
+				<ul class="footer-social__list">
+					<li class="footer-social__item">
+						<a class="footer-social__link" href="#">
+							<img src="img/instagram.svg" alt="">
+						</a>
+					</li>
+					<li class="footer-social__item">
+						<a class="footer-social__link" href="#">
+							<img src="img/twitter.svg" alt="">
+						</a>
+					</li>
+					<li class="footer-social__item">
+						<a class="footer-social__link" href="#">
+							<img src="img/facebook.svg" alt="">
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
---
+++ partials-front/menu.php
@@ -0,0 +1,63 @@
+<?php include('config/constants.php');?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Important to make website responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant Website</title>
+
+    <!-- Link our CSS file -->
+    <link rel="stylesheet" href="css_admin/style.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!-- Navbar Section Starts Here -->
+    <body>
+	<div class="wrapper">
+		<header class="header">
+    <div class="container">
+        <div class="header__inner">
+            <div class="header__top">
+                <a class="logo2" href="#">LOGO</a>
+                <a class="logo1" href="#">
+                    <img src="img/Logotip.svg" alt="">
+                </a>
+                <div class="basket-social">
+                    <a class="basket" href="#">
+                        <img src="img/basket.svg" alt="">
+                    </a>
+                    <a class="whatsapp" href="https://www.whatsapp.com/?lang=ru">
+                        <img src="img/whatsapp.svg" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="header__menu">
+                <div class="burger">
+                    <span class="burger__line burger__line-first"></span>
+                    <span class="burger__line burger__line-second"></span>
+                    <span class="burger__line burger__line-third"></span>
+                    <span class="burger__line burger__line-fourth"></span>
+                </div>
+                <nav class="menu">
+                    <ul class="menu__list">
+                        <li class="menu__list-item">
+                            <a class="menu__list-link" href="#">Каталог</a>
+                        </li>
+                        <li class="menu__list-item">
+                            <a class="menu__list-link" href="#">Выгодные предложения</a>
+                        </li>
+                        <li class="menu__list-item">
+                            <a class="menu__list-link" href="#">Рекомендации</a>
+                        </li>
+                        <li class="menu__list-item">
+                            <a class="menu__list-link" href="#">Наши приемущества</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
